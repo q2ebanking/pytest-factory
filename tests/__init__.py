23f7c52202f61f516375f4e39c25d727628ee555
Peter Kong <peter.kong@q2.com>
2022-03-15 18:22:08 -0500

    pathlib global search

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from configparser import ConfigParser
 from pytest_factory.framework.parse_configs import prep_stores_update_local
 
@@ -9,7 +10,11 @@ config = ConfigParser()
 
 # TODO this does not work with pycharm visual debugger which moves the CWD around so that this relative path breaks
 #  need to reimplement using pathlib.glob?
-config.read("tests/config.ini")
+p = Path()
+p_list = list(p.glob("**/config.ini"))
+if len(p_list) != 1:
+    raise Exception('config.ini is missing from project!')
+config.read(p_list[0])
 
 # Add to STORES for this test dir
 conf_dict = prep_stores_update_local(dir_name="default", conf=config)
