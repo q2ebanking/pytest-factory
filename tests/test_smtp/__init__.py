f5f8352c895e2dd79647b6b7c44025fda07c3035
Peter Kong <peter.kong@q2.com>
2022-04-25 12:19:33 -0500

    cleanup

--- tests/test_smtp/__init__.py
+++ tests/test_smtp/__init__.py
@@ -1,5 +1,3 @@
-from pytest_factory.framework.parse_configs import prep_stores_update_local, get_config_parser
-
-from pytest_factory.framework.mall import MALL
+from pytest_factory.framework.parse_configs import prep_stores_update_local
 
 prep_stores_update_local(dir_name=__name__.split('.')[1])
