eaf7775674f09ae6cf0e5943462f4aa32ae7d7f7
Peter Kong <peter.kong@q2.com>
2022-04-27 19:24:44 -0500

    cleanup

--- tests/test_configs/test_parse_configs.py
+++ tests/test_configs/test_parse_configs.py
@@ -1,4 +1,4 @@
-from pytest_factory.framework.parse_configs import prep_stores_update_local
+from pytest_factory.framework.parse_configs import prep_stores_update_local, DEFAULT_FOLDER_NAME
 from pytest_factory.framework.mall import MALL
 
 
@@ -7,9 +7,9 @@ def test_configs():
     assert MALL.current_test == 'test_configs'
     assert MALL._by_dir['test_configs']['string_var'] == 'BAR'
 
-    conf_dict = prep_stores_update_local(dir_name='tests')
-    assert conf_dict['tests']['string_var'] == 'FOO'
-    assert MALL._by_dir['tests']['string_var'] == 'FOO'
+    conf_dict = prep_stores_update_local(dir_name=DEFAULT_FOLDER_NAME)
+    assert conf_dict[DEFAULT_FOLDER_NAME]['string_var'] == 'FOO'
+    assert MALL._by_dir[DEFAULT_FOLDER_NAME]['string_var'] == 'FOO'
     assert MALL.string_var == 'BAR'
-    MALL._current_test_dir = 'tests'
+    MALL._current_test_dir = DEFAULT_FOLDER_NAME
     assert MALL.string_var == 'FOO'
